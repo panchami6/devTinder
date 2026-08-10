b2d7aa7a78e1f8c247da4063566b2ab21b3f05a8
Rakshith Kakathkar <rakskakathkar@gmail.com>
2026-08-10 20:33:44 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.use("/home", (req, res) => {
+  res.send("Hello from home");
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
